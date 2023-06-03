e514b291947a542a5fd8bba8a2bdba7fc008e7c0
haikelfazzani <haikel.fazzani@hotmail.com>
2023-06-03 19:00:37 +0100

    update dependencies

--- dev.ts
+++ dev.ts
@@ -1,6 +1,5 @@
 #!/usr/bin/env -S deno run -A --watch=static/,routes/
 
 import dev from "$fresh/dev.ts";
-import "https://deno.land/std@0.162.0/dotenv/load.ts";
 
 await dev(import.meta.url, "./main.ts");
